2e1343e5284a747e2ce46f452b6941f51f6ab8ab
Jeffrey Allen <jeffwayne256@gmail.com>
2024-01-10 09:38:52 -0600

    Added numpy

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+import numpy as np
+
 def main():
     print("Hello, world!")
+    a = np.array([1, 2, 3, 4, 5])
+    print(a[0])
 
 if __name__ == '__main__':
     main()
