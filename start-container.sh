6ed0f3e296436da0c1e35c9066db36dc22ed8e9c
Jeffrey Allen <jeffwayne256@gmail.com>
2024-03-16 11:09:04 -0500

    Changed container mountpoint and working directory

--- start-container.sh
+++ start-container.sh
@@ -4,7 +4,7 @@ REGISTRY_SERVER="docker.io"
 IMAGE_NAME="jeffwayne256/python-base-img"
 IMAGE_TAG="latest"
 
-BIND_MNT_CMD="-v $PWD:/usr/src -w /usr/src"
+BIND_MNT_CMD="-v $PWD:/workspace -w /workspace"
 
 CMD="/usr/local/bin/docker run -it --rm ${BIND_MNT_CMD} $REGISTRY_SERVER/$IMAGE_NAME:$IMAGE_TAG"
 
