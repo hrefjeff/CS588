50f111c4185564507051ac8dd0432c13506f5d80
Jeffrey Allen <jeffwayne256@gmail.com>
2024-03-17 21:55:53 -0500

    Added comparison of classifiers

--- iris_classification_models/iris-classification.py
+++ iris_classification_models/iris-classification.py
@@ -6,17 +6,59 @@ import numpy as np
 from sklearn import datasets
 from matplotlib import pyplot
 
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import StratifiedKFold
 
 from sklearn.datasets import load_iris
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import StratifiedKFold
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline
+
+
+def plot_learning_curve(classifier, X, y, steps=10, train_sizes=np.linspace(0.1, 1.0, 10), label="", color="r", axes=None):
+    estimator = Pipeline([("scaler", MinMaxScaler()), ("classifier", classifier)])
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+    train_scores = []
+    test_scores = []
+    train_sizes = []
+    for i in range(0, X_train.shape[0], X_train.shape[0] // steps):
+        if (i == 0):
+            continue
+        X_train_i = X_train[0:i, :]
+        y_train_i = y_train[0:i]
+        estimator.fit(X_train_i, y_train_i)
+        train_scores.append(estimator.score(X_train_i, y_train_i) * 100)
+        test_scores.append(estimator.score(X_test, y_test) * 100)
+        train_sizes.append(i + 1)
+
+    if (X_train.shape[0] % steps != 0):
+        estimator.fit(X_train, y_train)
+        train_scores.append(estimator.score(X_train, y_train) * 100)
+        test_scores.append(estimator.score(X_test, y_test) * 100)
+        train_sizes.append(X_train.shape[0])
+
+    if axes is None:
+        _, axes = plt.subplot(2)
+
+    train_s = np.linspace(10, 100, num=5)
+    axes[0].plot(train_sizes, test_scores, "o-", color=color, label=label)
+    axes[1].plot(train_sizes, train_scores, "o-", color=color, label=label)
+
+    print("Training Accuracy of ", label, ": ", train_scores[-1], "%")
+    print("Testing Accuracy of ", label, ": ", test_scores[-1], "%")
+    print("")
+    return plt
 
 def main():
     # Load Iris Data
@@ -60,6 +102,36 @@ def main():
     pyplot.boxplot(results, labels=names)
     pyplot.title('Algorithm Comparison')
     pyplot.savefig('algo-comparison.png')
+
+    _, axes = plt.subplots(1, 2, figsize=(12, 5))
+
+    num_steps = 10
+    classifier_labels = {
+        "Logistic Regression": (LogisticRegression(max_iter=1000, random_state=1), "red"),
+        "Random Forest": (RandomForestClassifier(random_state=1), "green"),
+        "SVM - Linear": (SVC(kernel="linear", random_state=1), "blue"),
+        "SVM - RBF": (SVC(kernel="rbf", random_state=1), "yellow"),
+        "SVM - Poly": (SVC(kernel="poly", random_state=1), "orange"),
+        "kNN": (KNeighborsClassifier(n_neighbors=5), "purple"),
+        "Guassian Naive Bayes": (GaussianNB(), "lime"),
+    }
+
+    for label in classifier_labels:
+        classifier = classifier_labels[label][0]
+        color = classifier_labels[label][1]
+        plot_learning_curve(classifier, X, y, steps=num_steps, label=label, color=color, axes=axes)
+
+    axes[0].set_xlabel('% of Training Exmples')
+    axes[0].set_ylabel('Overall Classification Accuracy')
+    axes[0].set_title('Model Evaluation - Cross-validation Accuracy')
+    axes[0].legend()
+
+    axes[1].set_xlabel('% of Training Exmples')
+    axes[1].set_ylabel('Training/Recall Accuracy')
+    axes[1].set_title('Model Evaluation - Training Accuracy')
+    axes[1].legend()
+
+    plt.savefig("comparison.png")
 
 if __name__ == '__main__':
     main()
